47decdaa0d4ec760f5d010859e7b5da4719a63b7
Rosemond Ho <rosemondho@gmail.com>
2021-03-09 16:39:36 +0000

    Finished wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,34 @@
-# put your code here.
+# Read file 
+# Tokenize the file by space
+# Make the dictionary from
+
+def find_words_count(filename):
+    words=[]
+    file=open(filename)
+
+    for line in file:
+        words.extend(line.rstrip().split(' '))
+
+    return words
+
+
+def create_dictionary(words):
+    word_count={}
+
+    for word in words:
+        # put the words in the dictionary
+        word = word.lower().strip('?!,.:()')
+        word_count[word] = word_count.get(word, 0) + 1
+
+    word_count = dict(sorted(word_count.items(), key=lambda kv: kv[1]))
+
+    for word, count in word_count.items():
+        print(word, count)
+    return word_count
+
+
+        
+
+
+words=find_words_count('test.txt')
+create_dictionary(words)
